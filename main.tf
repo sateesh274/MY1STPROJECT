fbcb74c2279ea2faa7675b86f2f50a348ab19cac
SATEESH <119327085+sateesh274@users.noreply.github.com>
2023-01-20 21:11:17 +0530

    Create main.tf
    abc commits

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIAVIT6LQWMEMX4RGG4"
+  secret_key = "bymUTDRa601/e6OYMmiAfeGguz/r6zPLhSCcPo4u"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0cca134ec43cf708f"
+  instance_type = "t2.micro"
+  key_name = "GITNEWPAIR"
+  availability_zone = "ap-south-1"
+  tags= {
+    Name = "sateesh-tf"
+  }
+}
