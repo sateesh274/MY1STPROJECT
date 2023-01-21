d0d1c681a55ad46e24dd7af64bea76e70f8f74b1
SATEESH <119327085+sateesh274@users.noreply.github.com>
2023-01-21 11:07:31 +0530

    Update main.tf
    abcdecommits

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-  region = "us-east-1b"
+  region = "us-east-1d"
   access_key = "AKIAVIT6LQWMEMX4RGG4"
   secret_key = "bymUTDRa601/e6OYMmiAfeGguz/r6zPLhSCcPo4u"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0cca134ec43cf708f"
+  ami           = "ami-0fe472d8a85bc7b0e"
   instance_type = "t2.micro"
   key_name = "GITNEWPAIR"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-east-1d"
   tags= {
     Name = "sateesh-tf"
   }
