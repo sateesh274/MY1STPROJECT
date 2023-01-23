a870cffbee7e598c700b1975d0c3cec26b56fddd
SATEESH <119327085+sateesh274@users.noreply.github.com>
2023-01-23 16:08:54 +0530

    Update main.tf
    abcde commits

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ provider "aws" {
   secret_key = "DDOZ9X5tv1JJ1d9mdKeiFrUwNDmRHIAGYz5R793E"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0cca134ec43cf708f"
+  resource "aws_instance" "example" {
+  ami = "ami-0cca134ec43cf708f"
   instance_type = "t2.micro"
   key_name = "GITNEWPAIR"
-  availability_zone = "ap-sougth-1a"
+  availability_zone = "ap-sougth-1"
   tags= {
     Name = "sateesh-tf"
   }
